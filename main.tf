985d98dfe16bb4df0963e7146a6fff817f058b90
juanjcsr <juanjcsr@gmail.com>
2023-05-11 20:33:09 -0600

    initial commit

---
+++ main.tf
@@ -0,0 +1,33 @@
+terraform {
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+    backend "s3" {
+        bucket = "terrastatejc"
+        key = "terrastate"
+        region = "us-west-2"
+    }
+    
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-08d70e59c07c61a3a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = var.instance_name
+  }
+
+
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "instance_name" {
+  type        = string
+  default     = "ExampleAppServerInstance"
+  description = "Value of the name tag for the EC2 instance"
+}
